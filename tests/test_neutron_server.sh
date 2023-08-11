2ed703da320857c4d138e12484c62b5bc368b993
Michael Sherman <shermanm@uchicago.edu>
2023-08-11 22:14:02 +0000

    [ci] add convenience test to check etcd3gw version
    for NGS batching to work, we need etcd3gw >= 1.0.1
    Add a simple test to check this in the built image

--- tests/test_neutron_server.sh
+++ tests/test_neutron_server.sh
@@ -4,9 +4,13 @@ set -euo pipefail
 
 # ensure patch 0001 is present
 docker run --rm -it \
-    ghcr.io/chameleoncloud/kolla/ubuntu-source-neutron-server:xena-dc5823b \
+    ghcr.io/chameleoncloud/kolla/ubuntu-source-neutron-server:xena \
     grep "startup-config\""  /var/lib/kolla/venv/lib/python3.8/site-packages/netmiko/dell/dell_force10_ssh.py
 
 docker run --rm -it \
-    ghcr.io/chameleoncloud/kolla/ubuntu-source-neutron-server:xena-dc5823b \
+    ghcr.io/chameleoncloud/kolla/ubuntu-source-neutron-server:xena \
     grep '"dell_fnioa": DellForce10SSH'  /var/lib/kolla/venv/lib/python3.8/site-packages/netmiko/ssh_dispatcher.py
+
+docker run --rm -it \
+    ghcr.io/chameleoncloud/kolla/ubuntu-source-neutron-server:xena \
+    pip show etcd3gw
