b048dc5f35dabd439a0c642f13b9cfc69ac8686a
Michael Sherman <shermanm@uchicago.edu>
2024-01-30 01:47:35 +0000

    attempt to fix horizon build, setting a workdir seems to break some tar security checking

--- run_the_build.sh
+++ run_the_build.sh
@@ -24,6 +24,7 @@ clone_branch_to_dir chameleoncloud/2023.1 https://github.com/ChameleonCloud/blaz
 clone_branch_to_dir chameleoncloud/2023.1 https://github.com/ChameleonCloud/blazar.git sources/blazar
 clone_branch_to_dir chameleoncloud/2023.1 https://github.com/ChameleonCloud/blazar-dashboard.git sources/blazar-dashboard
 clone_branch_to_dir chameleoncloud/2023.1 https://github.com/ChameleonCloud/blazar-nova.git sources/blazar-nova
+clone_branch_to_dir chameleoncloud/2023.1 https://github.com/ChameleonCloud/doni.git sources/doni
 clone_branch_to_dir chameleoncloud/2023.1 https://github.com/ChameleonCloud/heat.git sources/heat
 clone_branch_to_dir chameleoncloud/2023.1 https://github.com/ChameleonCloud/heat-dashboard.git sources/heat-dashboard
 clone_branch_to_dir chameleoncloud/2023.1 https://github.com/ChameleonCloud/horizon.git sources/horizon
@@ -31,27 +32,31 @@ clone_branch_to_dir master https://github.com/ChameleonCloud/horizon-theme.git s
 clone_branch_to_dir chameleoncloud/2023.1 https://github.com/ChameleonCloud/keystone.git sources/keystone
 clone_branch_to_dir chameleoncloud/2023.1 https://github.com/ChameleonCloud/nova.git sources/nova
 
-mkdir -p workdir
+locals_base_directory="$(pwd)/sources"
+
+
+# mkdir -p workdir
+# --work-dir workdir \
 kolla-build \
     --cache \
-    --work-dir workdir \
+    --locals-base "${locals_base_directory}" \
     --config-file kolla-build.conf \
     --template-override kolla-template-overrides.j2 \
-    --threads 40 \
-    --profile infra \
-    --profile blazar \
-    --profile cinder \
-    --profile glance \
-    --profile heat \
-    --profile horizon \
-    --profile ironic \
-    --profile keystone \
-    --profile manila \
-    --profile neutron \
-    --profile nova \
-    --profile placement \
-    --profile prometheus
-    # --profile doni \
+    --threads 1 \
+    --profile horizon
+    # --profile neutron \
+    # --profile blazar \
+    # --profile cinder \
+    # --profile keystone \
+    # --profile ironic \
+    # --profile nova \
+    # --profile placement
+    # --profile doni
+    # --profile infra \
+    # --profile glance \
+    # --profile heat \
+    # --profile manila \
+    # --profile prometheus
     # --profile tunelo \
     # --profile cyborg \
     # --profile zun \
