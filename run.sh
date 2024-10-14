00d671a056594eeb7f9924f49ce00c77ba0fd84c
Michael Sherman <shermanm@uchicago.edu>
2024-10-14 17:55:52 +0000

    auto tag containers with git sha

--- run.sh
+++ run.sh
@@ -2,8 +2,19 @@
 
 set -euo pipefail
 
+SHORT_SHA="$(git rev-parse --short HEAD)"
+
+# git index and files match HEAD
+if $(git diff-index --quiet HEAD --); then
+    DOCKER_TAG="${SHORT_SHA}"
+else
+    DOCKER_TAG="${SHORT_SHA}-dirty"
+fi
+
+echo "Tagging containers with ${DOCKER_TAG}"
 .venv/bin/kolla-build \
     --config-file kolla-build.conf \
     --template-override kolla-template-overrides.j2 \
+    --tag "${DOCKER_TAG}" \
     "$@"
 
