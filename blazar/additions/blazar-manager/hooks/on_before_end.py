ee83055d89bf48a920723881c4ce2d007d41c1d8
Mark Powers <mark@marks.kitchen>
2024-10-31 09:19:09 -0500

    Add more configuration options to blazar email hook (#43)
    * Add more configuration options to blazar email hook
    * Update to use blazar.conf for email

--- blazar/additions/blazar-manager/hooks/on_before_end.py
+++ blazar/additions/blazar-manager/hooks/on_before_end.py
@@ -72,10 +72,22 @@ def render_template(**kwargs):
     return templ.render(**kwargs)
 
 
-def send_email(email_host, to, sender, cc=None, bcc=None, subject=None, body=None):
+def send_email(email_host, email_port, email_ssl, email_user, email_password, to, sender, cc=None, bcc=None, subject=None, body=None):
     # convert TO into list if string
     if type(to) is not list:
         to = to.split()
+
+    try:
+        email_port = int(email_port)
+    except ValueError:
+        print(f"Invalid value for smtp port, expected int: {email_port}")
+        sys.exit(1)
+
+    try:
+        email_ssl = bool(email_ssl)
+    except ValueError:
+        print(f"Invalid value for smtp ssl, expected bool: {email_ssl}")
+        sys.exit(1)
 
     to_list = [addr for addr in (to + [cc] + [bcc]) if addr is not None]
 
@@ -88,7 +100,12 @@ def send_email(email_host, to, sender, cc=None, bcc=None, subject=None, body=Non
     msg.attach(MIMEText(body, "html"))
 
     # send email
-    server = smtplib.SMTP(email_host, timeout=30)
+    if email_ssl:
+        server = smtplib.SMTP_SSL(email_host, email_port, timeout=30)
+    else:
+        server = smtplib.SMTP(email_host, email_port, timeout=30)
+    if email_user and email_password:
+        server.login(email_user, email_password)
     server.sendmail(sender, to_list, msg.as_string())
     server.quit()
 
@@ -146,13 +163,23 @@ def main(argv):
 
     # read email host from blazar.conf
     email_host = "127.0.0.1"
+    # smtplib's default is 0, defaults to OS implementation
+    email_port = 0
+    email_ssl = False
+    email_user = None
+    email_password = None
     blazar_config = configparser.ConfigParser()
     try:
         blazar_config.read("/etc/blazar/blazar.conf")
         email_host = blazar_config["physical:host"]["email_relay"]
+        # smtplib's default is 0, defaults to OS implementation
+        email_port = blazar_config["physical:host"].get("email_port", 0)
+        email_ssl = blazar_config["physical:host"].get("email_ssl", False)
+        email_user = blazar_config["physical:host"].get("email_user")
+        email_password = blazar_config["physical:host"].get("email_password")
     except Exception:
         pass
-    send_email(email_host, args.to, args.sender, args.cc, args.bcc, subject, html)
+    send_email(email_host, email_port, email_ssl, email_user, email_password, args.to, args.sender, args.cc, args.bcc, subject, html)
 
 
 if __name__ == "__main__":
