372d372f7f04b368c6919c0d867e3b6911903f06
Michael Sherman <shermanm@uchicago.edu>
2024-11-04 23:56:56 +0000

    fixup gh ci

--- run.sh
+++ run.sh
@@ -11,10 +11,21 @@ else
     DOCKER_TAG="${SHORT_SHA}-dirty"
 fi
 
-echo "Tagging containers with ${DOCKER_TAG}"
-.venv/bin/kolla-build \
-    --config-file kolla-build.conf \
-    --template-override kolla-template-overrides.j2 \
-    --tag "${DOCKER_TAG}" \
-    "$@"
 
+BUILD_PROFILE=${BUILD_PROFILE:-}
+BUILD_PATTERN=${BUILD_PATTERN:-}
+BUILD_TAG=${BUILD_TAG:-$DOCKER_TAG}
+PUSH=${PUSH:-}
+
+# handle env vars from CI
+CMD=".venv/bin/kolla-build \
+    --config-file kolla-build.conf \
+    --template-override kolla-template-overrides.j2"
+
+if [ "$PUSH" = "true" ]; then CMD="$CMD --push"; fi
+if [ ! -z "$BUILD_PROFILE" ]; then CMD="$CMD --profile $BUILD_PROFILE"; fi
+if [ -n "$BUILD_TAG" ]; then CMD="$CMD --tag $BUILD_TAG"; fi
+if [ -n "$BUILD_PATTERN" ]; then CMD="$CMD $BUILD_PATTERN"; fi
+
+echo "Invoking $CMD $@"
+$CMD "$@"
