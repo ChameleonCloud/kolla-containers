b820fd8df1ad17a7607c13386d6ddcba34d3be51
Michael Sherman <shermanm@uchicago.edu>
2026-07-01 17:50:01 +0000

    fix pkg_resources missing

--- install.sh
+++ install.sh
@@ -7,5 +7,9 @@ git submodule update --init
 
 # create venv for tool installation
 python3 -m venv .venv
-.venv/bin/pip install -e src/kolla
-
+PIP_BUILD_CONSTRAINT=build-constraints.txt \
+.venv/bin/pip install \
+    -c https://raw.githubusercontent.com/openstack/requirements/refs/heads/unmaintained/2024.1/upper-constraints.txt \
+    -c build-constraints.txt \
+    -e src/kolla \
+    docker
