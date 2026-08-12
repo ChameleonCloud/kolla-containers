a111b0e40930cc2f7ef485d9bc2e3ef049a926a3
Michael Sherman <shermanm@uchicago.edu>
2026-08-12 16:58:22 +0000

    feat: support setting base_tag via run.sh
    In case we need to build multiple variants, such as ubuntu 22.04/24.04,
    pass kolla base_tag through run.sh

--- run.sh
+++ run.sh
@@ -15,6 +15,8 @@ fi
 BUILD_PROFILE=${BUILD_PROFILE:-}
 BUILD_PATTERN=${BUILD_PATTERN:-}
 BUILD_TAG=${BUILD_TAG:-$DOCKER_TAG}
+# unset falls through to base_tag in kolla-build.conf
+BASE_TAG=${BASE_TAG:-}
 PUSH=${PUSH:-}
 
 # handle env vars from CI
@@ -25,6 +27,7 @@ CMD=".venv/bin/kolla-build \
 if [ "$PUSH" = "true" ]; then CMD="$CMD --push"; fi
 if [ ! -z "$BUILD_PROFILE" ]; then CMD="$CMD --profile $BUILD_PROFILE"; fi
 if [ -n "$BUILD_TAG" ]; then CMD="$CMD --tag $BUILD_TAG"; fi
+if [ -n "$BASE_TAG" ]; then CMD="$CMD --base-tag $BASE_TAG"; fi
 if [ -n "$BUILD_PATTERN" ]; then CMD="$CMD $BUILD_PATTERN"; fi
 
 echo "Invoking $CMD $@"
